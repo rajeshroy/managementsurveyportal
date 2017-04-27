f92afed62dc00df99d5909025dfc761b828054dc
Kumar Ashutosh <kumarashutosh.17.1993@gmail.com>
2017-04-27 07:47:25 +0000

    [DEV] .panel design

--- webapp/controller/MDList.controller.js
+++ webapp/controller/MDList.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-		"sap/ui/core/routing/History"
-], function(Controller, History) {
+	"mana/survey/portal/controller/BaseController",
+	"sap/ui/core/routing/History",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, History, Filter, FilterOperator, JSONModel) {
 	"use strict";
 
 	return Controller.extend("mana.survey.portal.controller.MDList", {
@@ -11,13 +14,90 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf mana.survey.portal.view.MDList
 		 */
-			onInit: function() {
-				this.getRouter().getRoute("manager").attachPatternMatched(this._onObjectMatched, this);
-			}
+		onInit: function() {
+			var oJSONModel = new JSONModel();
+			this.setModel(new JSONModel(), "QuestionList");
+			this.setModel(oJSONModel, "managerList");
+			this.getRouter().getRoute("manager").attachPatternMatched(this._onObjectMatched, this);
+		},
+
+		_onObjectMatched: function(oEvent) {
+			var aFilter, mParameter, sCustomerId, sRating, oArguments;
 			
-		//	_onObjectMatched: function(oEvent){
-			//	var	sObjectId = oEvent.getParameter("arguments").objectId;
-		//	}
+			oArguments = oEvent.getParameter("arguments");
+			sCustomerId = oArguments.custId;
+			sRating = oArguments.objectId;
+			aFilter = this._prepareFilter(sCustomerId, sRating);
+			mParameter ={
+			context: null,
+			/*urlParameters: {
+				expand:'CustFeedbackSet'
+			},*/
+			filters: aFilter,
+			success: jQuery.proxy(this.fnSuccessMListLoad, this),
+			error: jQuery.proxy(this.fnErrorMListLoad, this)
+			};
+			this.getModel().read("/CustAvgFeedbackSet", mParameter);
+
+		},
+		/**
+		 * 
+		 * 
+		 */
+		 fnSuccessMListLoad: function(oSuccessData){
+			this.getModel("managerList").setProperty("/", oSuccessData.results);	
+		 },
+		 /**
+		  * 
+		  * 
+		  */
+		  fnErrorMListLoad: function(oError){
+		  	
+		  },
+		/**
+		 * This function is used to prepare the filter object
+		 * @param {string} sCustomerId Customer Id
+		 * @param {string} sRating customer avg. rating
+		 * @returns {object} oFilter
+		 */ 
+		 _prepareFilter: function(sCustomerId, sRating){
+		 	var aFilter =[ 
+		 	new Filter("Customerid", FilterOperator.EQ, sCustomerId),
+		 	new Filter("FeedbackAvgFd", FilterOperator.EQ, sRating)
+		 	];
+		 	return aFilter;
+		 },
+		 
+		 /**
+		  * 
+		  * 
+		  */
+		  onManagerExpand: function(oEvent){
+		  	var mParameter, sManagetId = oEvent.getSource().getHeaderText(),
+		  	aFilter = [new Filter("Managerid", FilterOperator.EQ, sManagetId)],
+		  	oModelManager = this.getModel("managerList").oData;
+		  	mParameter ={
+			context: null,
+			/*urlParameters: {
+				expand:'CustFeedbackSet'
+			},*/
+			filters: aFilter,
+			success: jQuery.proxy(this.fnSuccessQuesList, this),
+			error: jQuery.proxy(this.fnErrorQuesList, this)
+			};
+			this.getModel().read("/CustFeedbackSet", mParameter);
+			
+		
+		  	
+		  },
+		  
+		  /**
+		   * 
+		   * 
+		   */
+		   fnSuccessQuesList: function(oSuccessData){
+		   	 this.getModel("QuestionList").setProperty("/", oSuccessData.results);
+		   }
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
